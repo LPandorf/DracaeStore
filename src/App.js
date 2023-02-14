bc4484937166e3cd4e281926bfb1437bccbe6357
LPandorf <leticiapandorf@gmail.com>
2023-02-13 23:53:37 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import styled from 'styled-components';
+
+export default function App() {
+  return (
+    <Wrapper>
+            Home
+        </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+color: black;
+    width: 100vw;
+    height: 100vh;
+    background-color: crimson;
+    top: 0;
+    left: 0;
+`;
